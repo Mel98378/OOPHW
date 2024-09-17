591adce0be4f8c72e3cbdfafbce036c3381a9e13
Jershw43 <95701884+jershw43@users.noreply.github.com>
2024-09-17 15:57:52 -0400

    initial commit

---
+++ Program1/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "student.h"
+
+using namespace std;
+
+int main(void)
+{
+  Fraction fraction1;
+
+  fraction1.setsign('+');
+  fraction1.setwhole(2);
+  fraction1.setnum(3);
+  fraction1.setden(4);
+  fraction1.display();
+
+  system("PAUSE");
+  return 0;
+}
